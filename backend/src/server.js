a45b706655b79a314980d79c34c500cf98047381
Muskaan04Puri <muskanpuri0410@gmail.com>
2026-03-11 19:13:20 +0530

    backend, db and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = ENV.PORT;
 
 app.get("/", (req, res) => {
     res.status(200).json({ msg: "success from backend" });
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
